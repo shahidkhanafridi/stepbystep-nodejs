3d51e80f8c2ca75a43b0903a8afb236853e80eee
Muhammad Shahid Khan Afridi <muhammadshahidkhanafridi@gmail.com>
2021-06-12 23:15:53 +0500

    appendFile of FileStream
    appendFileSync used to add more text to existing file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
 //Creating new file
-fs.writeFileSync('read.txt',"Welcome to NodeJS tutorial");
+//fs.writeFileSync('read.txt',"Welcome to NodeJS tutorial");
 
+// above line commented as we already created the file with FileStream,
+// Now I need to add more text to the file, this can be doce using appednFile function as below:
+
+fs.appendFileSync('read.txt'," Another line of text need to be added.");
